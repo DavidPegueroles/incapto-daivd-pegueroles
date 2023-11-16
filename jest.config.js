d86a5dbbacc9e1d2d1dad86559b396b98419f4f4
DavidPegueroles <david.peguerolesbel@gmail.com>
2023-11-16 21:11:07 +0100

    Adds supertest dependency and adds new script for checking coverage

--- jest.config.js
+++ jest.config.js
@@ -1,5 +1,10 @@
 /** @type {import('ts-jest').JestConfigWithTsJest} */
 module.exports = {
-  preset: 'ts-jest',
-  testEnvironment: 'node',
+  preset: "ts-jest",
+  testEnvironment: "node",
+  collectCoverageFrom: [
+    "src/**/*.ts",
+    "!src/index.ts",
+    "!src/server/initialiseServer.ts",
+  ],
 };
